04cdaba5c60d108590327a6635709c464e7a8e62
player-001 <mohitworksop@gmail.com>
2026-04-16 12:37:28 -0400

    updated

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,10 +1,13 @@
 import React, { useState, useEffect } from 'react';
+import {useSelector, useDispatch} from "react-redux"
+import {addItem} from "./CartSlice"
 import './ProductList.css'
 import CartItem from './CartItem';
 function ProductList({ onHomeClick }) {
     const [showCart, setShowCart] = useState(false);
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
-
+    const cart = useSelector(state => state.cart.items)
+    const dispatch = useDispatch()
     const plantsArray = [
         {
             category: "Air Purifying Plants",
@@ -232,7 +235,16 @@ function ProductList({ onHomeClick }) {
         fontSize: '30px',
         textDecoration: 'none',
     }
-
+    const Totalquantity = () => {
+        if (cart){
+        let total = 0
+        cart.forEach((item) => total + item.quantity)
+        return total
+        }
+        else{
+            return 0
+        }
+    }
     const handleHomeClick = (e) => {
         e.preventDefault();
         onHomeClick();
@@ -245,6 +257,7 @@ function ProductList({ onHomeClick }) {
     const handlePlantsClick = (e) => {
         e.preventDefault();
         setShowPlants(true); // Set showAboutUs to true when "About Us" link is clicked
+        dispatch(addItem(e.name))
         setShowCart(false); // Hide the cart when navigating to About Us
     };
 
@@ -272,9 +285,10 @@ function ProductList({ onHomeClick }) {
                     <div> <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}><h1 className='cart'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path></svg></h1></a></div>
                 </div>
             </div>
+
             {!showCart ? (
                 <div className="product-grid">
-
+                    
 
                 </div>
             ) : (
